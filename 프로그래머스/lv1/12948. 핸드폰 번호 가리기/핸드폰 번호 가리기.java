cf5303a2096bb4c93c345a57985dd6bbbf9c66a0
bakzihun <128371819+bbakzi@users.noreply.github.com>
2023-04-08 14:16:39 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.01 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,13 +1,13 @@
+//String substring(int index) 풀이
+//1. phone_number.length()-4 의 길이를 구하고 answer에 넣는다.
+//2. substring 으로 뒤에서 4~끝까지를 가지고 와서 answer에 넣는다.
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
-        for(int i=0;i<phone_number.length();i++){
-            if(i<phone_number.length()-4){
-                answer+='*';
-            }else {
-                answer+=phone_number.charAt(i); //charAt() 문자열에서 ()안의 문자를 반환한다. : 스트링의 기능 활용
-            }
-        }
+        for (int i=0; i<phone_number.length()-4;i++)
+            answer+='*';
+        answer+= phone_number.substring(phone_number.length()-4);
+        //String.substring(int index값 0부터 시작)
         return answer;
     }
 }
